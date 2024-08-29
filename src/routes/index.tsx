3d61ff9a8e93a2baaf5159a14cfd31bab4207ce3
Jhoão <jhpdrnsantos2@gmail.com>
2024-08-29 13:30:49 -0300

    feat: Criação das rotas do blog

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Route, Navigate, Routes as Switch} from 'react-router-dom';
+import { Login } from '../pages/login';
+import { Register } from '../pages/register';
+import { Dashboard } from '../pages/dashboard';
+
+export const Routes = () =>{
+    const auth = true
+    return(
+        <Switch>
+            <Route path='/' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/dashboard' element={auth ? <Dashboard/>: <Navigate to='/' replace /> } />
+
+        </Switch>
+    );
+}
